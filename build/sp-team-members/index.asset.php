91b6ea36a2b37dc617da597af282bbc5c6624150
Mohammad Ullah <ullahmohammad314@gmail.com>
2025-12-30 17:55:08 +0600

    Add @wordpress/blob dependency and enhance team member block functionality
    - Added @wordpress/blob dependency to package.json and package-lock.json.
    - Updated editor.scss to improve styling for team member images, including centering text and adding border-radius to images.
    - Enhanced edit.js to handle image loading states with a spinner and improved URL handling for images.

--- build/sp-team-members/index.asset.php
+++ build/sp-team-members/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f7b090800ef74e9414df');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6176197c9660193c6856');
--- src/sp-team-members/team-member/edit.js
+++ src/sp-team-members/team-member/edit.js
@@ -4,6 +4,8 @@ import {
 	useBlockProps,
 } from "@wordpress/block-editor";
 import { __ } from "@wordpress/i18n";
+import { isBlobURL } from "@wordpress/blob";
+import { Spinner } from "@wordpress/components";
 
 export default function Edit({ attributes, setAttributes }) {
 	const { name, bio, url, id, alt } = attributes;
@@ -23,13 +25,26 @@ export default function Edit({ attributes, setAttributes }) {
 		setAttributes({ url: image.url, id: image.id, alt: image.alt });
 	};
 
+	const onSelectUrl = (newUrl) => {
+		setAttributes({ url: newUrl, id: undefined, alt: "" });
+	};
+
 	return (
 		<div {...useBlockProps()}>
-			{url && <img src={url} alt={alt} />}
+			{url && (
+				<div
+					className={`wp-block-create-block-sp-team-member-img${
+						isBlobURL(url) ? " is-loading" : ""
+					}`}
+				>
+					<img src={url} alt={alt} />
+					{isBlobURL(url) && <Spinner />}
+				</div>
+			)}
 			<MediaPlaceholder
 				icon="admin-users"
 				onSelect={onSelectImage}
-				onSelectURL={(val) => console.log(val)}
+				onSelectURL={onSelectUrl}
 				onError={(val) => console.log(val)}
 				accept="image/*"
 				allowedTypes={["image"]}
